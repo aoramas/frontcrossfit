394b44a036ea85b3ece4724aa6d37f21ebed7651
Cesar Tipan <cesar.tipan@bitekso.com>
2023-09-13 15:45:21 -0500

    home con menu lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HorariosComponent } from './pages/horarios/horarios.component';
+import { WodsComponent } from './pages/wods/wods.component';
+import { EjerciciosComponent } from './pages/ejercicios/ejercicios.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HorariosComponent,
+    WodsComponent,
+    EjerciciosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/wods/wods.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wods',
+  templateUrl: './wods.component.html',
+  styleUrls: ['./wods.component.scss']
+})
+export class WodsComponent {
+
+}
