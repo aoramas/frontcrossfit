ee4ad33ef96f2cec019b90f3e7386294bb637a7e
CesarAlex87 <cesar.tipanant@ug.edu.ec>
2023-09-13 23:17:33 -0500

    Enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HorariosComponent } from './pages/horarios/horarios.component';
+import { WodsComponent } from './pages/wods/wods.component';
+import { EjerciciosComponent } from './pages/ejercicios/ejercicios.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'horarios', component: HorariosComponent
+  },
+  {
+    path: 'wods', component: WodsComponent
+  },
+  {
+    path: 'ejercicios', component: EjerciciosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HorariosComponent } from './pages/horarios/horarios.component';
-import { WodsComponent } from './pages/wods/wods.component';
-import { EjerciciosComponent } from './pages/ejercicios/ejercicios.component';
-import { HomeComponent } from './pages/home/home.component';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MenuComponent } from './components/menu/menu.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HorariosComponent,
-    WodsComponent,
-    EjerciciosComponent,
-    HomeComponent,
     NavbarComponent,
     MenuComponent
   ],
