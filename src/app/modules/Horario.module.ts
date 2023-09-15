00ca8f2d236aa74ff97fa53c325a4bce2bc32e67
CesarAlex87 <cesar.tipanant@ug.edu.ec>
2023-09-14 22:12:03 -0500

    modules Wod y Horario creados

---
+++ src/app/modules/Horario.module.ts
@@ -0,0 +1,6 @@
+export interface Horario {
+    id: number;
+    diaSemana: string;
+    horaInicio: string;
+    horaFin: string;
+}
---
+++ src/app/modules/Wod.module.ts
@@ -0,0 +1,5 @@
+export interface Wod {
+  id: string;
+  ejercicio: string;
+  repeticion: string;
+}
